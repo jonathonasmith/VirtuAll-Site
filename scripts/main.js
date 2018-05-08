12bb224c0d0d8da59d2d39c32761c3c922924f4d
Ryan Otto <rotto@drew.edu>
2018-05-08 00:30:07 -0400

    Changed navbar styling, added active toggle code
    * Navbar now has black/white/red color scheme
    * Clicking one link will change the others back to inactive state and the clicked link to the active state, causing it to highlight in red

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 
 	$(function(){ $("body").niceScroll(); });
-	
+	$('#nav1').click(function(e) { 
+		$('#nav1').attr('class','active');
+		$('#nav2').attr('class','inactive');
+		$('#nav3').attr('class','inactive');
+		$('#nav4').attr('class','inactive');
+	});
+	$('#nav2').click(function(e) { 
+		$('#nav2').attr('class','active');
+		$('#nav1').attr('class','inactive');
+		$('#nav3').attr('class','inactive');
+		$('#nav4').attr('class','inactive');
+	});
+	$('#nav3').click(function(e) { 
+		$('#nav3').attr('class','active');
+		$('#nav1').attr('class','inactive');
+		$('#nav2').attr('class','inactive');
+		$('#nav4').attr('class','inactive');
+	});
+	$('#nav4').click(function(e) { 
+		$('#nav4').attr('class','active');
+		$('#nav1').attr('class','inactive');
+		$('#nav2').attr('class','inactive');
+		$('#nav3').attr('class','inactive');
+	});
+
 });
