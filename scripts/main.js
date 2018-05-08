6c16d9c876c3747b34e8a4dbc1fb95675ff8c38e
Ryan Otto <rotto@drew.edu>
2018-05-08 00:51:43 -0400

    Added scroll on click to navbar
    When clicking relevant navbar elements, screen will now scroll to corresponding div

--- scripts/main.js
+++ scripts/main.js
@@ -6,24 +6,28 @@ $(document).ready(function() {
 		$('#nav2').attr('class','inactive');
 		$('#nav3').attr('class','inactive');
 		$('#nav4').attr('class','inactive');
+		$('html, body').animate({ scrollTop: $("#about1").offset().top-90 }, 1000);
 	});
 	$('#nav2').click(function(e) { 
 		$('#nav2').attr('class','active');
 		$('#nav1').attr('class','inactive');
 		$('#nav3').attr('class','inactive');
 		$('#nav4').attr('class','inactive');
+		$('html, body').animate({ scrollTop: $("#about2").offset().top-90 }, 1000);
 	});
 	$('#nav3').click(function(e) { 
 		$('#nav3').attr('class','active');
 		$('#nav1').attr('class','inactive');
 		$('#nav2').attr('class','inactive');
 		$('#nav4').attr('class','inactive');
+		$('html, body').animate({ scrollTop: $("#about3").offset().top-90 }, 1000);
 	});
 	$('#nav4').click(function(e) { 
 		$('#nav4').attr('class','active');
 		$('#nav1').attr('class','inactive');
 		$('#nav2').attr('class','inactive');
 		$('#nav3').attr('class','inactive');
+		$('html, body').animate({ scrollTop: $("#about4").offset().top-90 }, 1000);
 	});
 
 });
