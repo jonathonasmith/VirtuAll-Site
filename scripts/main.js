99ae05f3590e531f7e50090414e5cea5345efa94
Ryan Otto <rotto@drew.edu>
2018-05-13 13:37:05 -0400

    Imported fullPage.js, start of demo edit

--- scripts/main.js
+++ scripts/main.js
@@ -50,3 +50,8 @@ $(document).ready(function() {
 	});
 
 });
+$(document).ready(function(){
+	$("#demosMenu").change(function(){
+	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
+	});
+});
