bca6b9e5c95ea1238e937c072068f50ffe7335d3
Jonathon Allaire Smith <jonathonasmith@gmail.edu>
2018-07-20 19:02:24 -0400

    Style Edits
    Pushed to live site.
    Also Cleaned out unused photos,
    Added ScrollBar
    Removed WhatWeDo

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@ $('#fullpage').fullpage({
 		autoScrolling: true,
 		fitToSection: true,
 		fitToSectionDelay: 1000,
-		scrollBar: false,
+		scrollBar: true,
 		easing: 'easeInOutCubic',
 		easingcss3: 'ease',
 		loopBottom: false,
